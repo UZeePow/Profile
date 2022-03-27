3b83a46ac75596d4e05cec1c9023fae9c528f254
UZeePow <84755801+UZeePow@users.noreply.github.com>
2022-03-27 14:03:13 -0700

    Separated resume tab from home

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-    console.log("Hello, world!");
+//Get the button:
+let topBtn = document.getElementById("topBtn");
+let color = document.getElementById("color")
+let imgtag = "";
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+function gotoTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function onFileSelected(event) {
+  var file = event.target.files[0];
+  var r = new FileReader();
+
+  imgtag = document.getElementById("fileImg");
+  imgtag.title = file.name;
+
+  r.onload = function (event) {
+    imgtag.src = event.target.result;
+  };
+
+  r.readAsDataURL(file);
+  loadNewColor();
+}
+
+function loadNewColor() {
+  $.ajax({
+    url: "/getColors",
+    type: "POST",
+    dataType: "json",
+    data: { param: imgtag },
+  }).done(function (data) {
+    $(color).replaceWith(data);
+  });
+}
